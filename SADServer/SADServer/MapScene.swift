cf89ee40c93cb2fcca106f476654e376ee58319a
Richard Marquez <rmarquez14@winona.edu>
2015-03-28 12:32:28 -0500

    can add arbitrary points to map

--- SADServer/SADServer/MapScene.swift
+++ SADServer/SADServer/MapScene.swift
@@ -11,12 +11,27 @@ import SpriteKit
 
 class MapScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        start()
+        addRandomPoints()
     }
     
-    func start() {
-        println("starting")
+    func addRandomPoints() {
+        addPoint(getRandomPosition())
+        
+        delay(1) {
+            self.addRandomPoints()
+        }
+    }
+    
+    func addPoint(loc: CGPoint) {
+        var color = NSColor(hex: "00ff00")
+        var size = CGSize(width: 5, height: 5)
+        
+        let point = SKSpriteNode(color: color, size: size)
+        
+        point.position = loc
+        
+        self.addChild(point)
+
     }
 
     
